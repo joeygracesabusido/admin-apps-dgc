0fd3e30f41eaee1fe440d2ab8c4d5b7956372159
jerome <joeysabusido@ubuntuserverdgc.homenet.dgc>
2024-10-08 16:46:45 +0800

    10.08.2024 4:45 PM

--- apps/routes/repair.py
+++ apps/routes/repair.py
@@ -7,8 +7,8 @@ from fastapi.responses import JSONResponse
 
 from pydantic import BaseModel
 
-#from  ..database.mongodb import create_mongo_client
-#mydb = create_mongo_client()
+from  ..database.mongodb import create_mongo_client
+mydb = create_mongo_client()
 
 #from bson import ObjectId
 
@@ -17,7 +17,51 @@ from  ..authentication.authenticate_user import get_current_user
 api_repair= APIRouter(include_in_schema=False)
 templates = Jinja2Templates(directory="apps/templates")
 
+class RepairItem(BaseModel):
+    company: str
+    srs: Optional[str]
+    repair_date: Optional[datetime] = None
+    brans: Optional[str] = None
+    model: Optional[str] = None
+    serial_number: Optional[str] = None
+    remarks: Optional[float] = None
+    repair_user: Optional[str] = None
+    amount: Optional[float] = None
+    department: Optional[str] = None
+    date_updated: Optional[datetime] = None
+    date_created: Optional[datetime] = None
+
+
+
+
+
+
 
 @api_repair.get("/api-repair/", response_class=HTMLResponse)
 async def api_ticketing(request: Request,username: str = Depends(get_current_user)):
     return templates.TemplateResponse("repair/printer_repair.html", {"request": request})
+
+
+
+@api_repair.post('/api-insert-repair-item')
+async def insert_inventory_item(items:RepairItem, username: str = Depends(get_current_user)):
+     dataInsert = dict()
+     dataInsert = {
+         "company": items.company,
+         "srs": items.srs,
+         "repair_date": items.repair_date,
+         "brand": items.brand,
+         "model": items.model,
+         "serial_number": items.serial_number,
+         "remarks": items.remarks,
+         "repair_user":items.repair_user,
+         "amount":items.amount,
+         "department": items.department,
+         "user": username,
+         "date_created": datetime.now().isoformat(),
+         "date_updated": items.date_updated.isoformat() if items.date_updated else None
+         }
+     mydb.repair.insert_one(dataInsert)
+     return {"message":"Data has been save"} 
+
+
