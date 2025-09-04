b44b770909cec239f23c82a606b53a637c7dc119
joeygracesabusido <joeygracesabusido@gmail.com>
2025-09-04 13:51:53 +0300

    09.04.2025 1:51 PM

--- apps/routes/supply.py
+++ apps/routes/supply.py
@@ -22,6 +22,10 @@ templates = Jinja2Templates(directory="apps/templates")
 async def api_ticketing(request: Request,username: str = Depends(get_current_user)):
     return templates.TemplateResponse("inventory_supply/invt_supplier_enhanced.html", {"request": request})
 
+# @api_supplier_temp.get("/inventory-supply", response_class=HTMLResponse)
+# async def api_inventory_supply(request: Request, username: str = Depends(get_current_user)):
+#     return templates.TemplateResponse("inventory_supply/inventory_supply.html",{"request":request})
+
 @api_supplier_temp.get("/inventory-supply", response_class=HTMLResponse)
 async def api_inventory_supply(request: Request, username: str = Depends(get_current_user)):
     return templates.TemplateResponse("inventory_supply/inventory_supply_clean.html",{"request":request})
